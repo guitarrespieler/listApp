a6ee5f14bd33a8dcf98ff06f9495984a9e287dfd
Zsiga Tibor <tibor.zsiga@hotmail.com>
2018-04-27 21:05:45 +0200

    menu added

--- app/src/main/java/hu/bme/aut/listapp/MainActivity.java
+++ app/src/main/java/hu/bme/aut/listapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -48,5 +50,12 @@ public class MainActivity extends AppCompatActivity {
         }
         if(intent != null)
             startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 }
